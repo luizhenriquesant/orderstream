681f092c4b7fb5daec93e961c0acc3828102b844
lhsfreitas <lhs.freitas@unesp.br>
2026-02-07 17:00:19 -0300

    feat entrypoint.ts created

---
+++ apps/order-command-api/src/entrypoint.ts
@@ -0,0 +1,12 @@
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
