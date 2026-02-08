c8224b9e9cba8fc19d96aa100503c313fdb2f6e6
lhsfreitas <lhs.freitas@unesp.br>
2026-02-08 17:06:24 -0300

    feat kafka start

---
+++ apps/order-command-api/src/infrastructure/messaging/kafka.producer.ts
@@ -0,0 +1,26 @@
+import { Producer } from 'kafkajs';
+
+export class KafkaProducer {
+    public constructor(
+        private readonly producer: Producer
+    ) { }
+    async connect(): Promise<void> {
+        await this.producer.connect();
+    };
+
+    async publish(
+        topic: string,
+        key: string,
+        value: string
+    ): Promise<void> {
+        const messages = [{ key: key, value: value }]
+        await this.producer.send({
+            topic,
+            messages
+        })
+    }
+
+    async disconnect(): Promise<void> {
+        await this.producer.disconnect();
+    }
+}
